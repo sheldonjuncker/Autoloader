970da5c91d7021a2aa670b64fb9520d6095060b2
Sheldon Juncker <sheldonjuncker@gmail.com>
2017-01-11 23:16:01 -0500

    Adding initial files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Autoloader
+This is what I believe is a PSR-4 compatible autoloader.
+However, I don't have the expertise to guarantee that it is indeed fully PSR-4 compliant.
+I use it in my own projects. Use it at your own risk!
---
+++ src/Autoload.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+* Loads a simple PSR-4 autoloader for the Validator.
+*/
+spl_autoload_register(function(string $className){
+	//A mapping of namespace prefixes to base directories.
+	$prefixes = [
+		'Validator' => 'src\\Validator',
+	];
+
+	//Get list of namespaces, and unqualified class name
+	$namespaces = explode('\\', $className);
+	$class = array_pop($namespaces);
+
+	//Find the longest prefix to use as the base path
+	$basePath = "";
+	$pathParts = [];
+	while(count($namespaces))
+	{
+		$prefixStr = implode('\\', $namespaces);
+		if(isset($prefixes[$prefixStr]))
+		{
+			$basePath = $prefixes[$prefixStr];
+			break;
+		}
+
+		else
+		{
+			array_unshift($pathParts, array_pop($namespaces));
+		}
+	}
+
+	//Found
+	if($basePath != "")
+	{
+		array_unshift($pathParts, $basePath);
+		array_push($pathParts, $class . '.php');
+		$file = implode(DIRECTORY_SEPARATOR, $pathParts);
+		if(file_exists($file))
+		{
+			require $file;
+		}
+	}
+});
+
+?>
